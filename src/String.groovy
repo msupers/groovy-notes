b4a49069f247c73d015aada9a537d59cfbc1d91e
zhangmiao334 <zhangmiao334@xdf.cn>
2020-11-09 20:21:03 +0800

    update

---
+++ src/String.groovy
@@ -0,0 +1,13 @@
+def   s1 = 'xxxxx'
+println s1
+
+def  s2 = '''\
+line 1
+line 2
+line 3
+'''
+println s2
+
+
+def s3 = 'i  \'m zhangmiao'
+println s3
---
+++ src/importExample.groovy
@@ -0,0 +1,12 @@
+import groovy.xml.MarkupBuilder
+
+def xml = new MarkupBuilder();
+
+def Boolean aaa = false;
+
+println aaa;
+
+for (int i=0; i<=10; i++) {
+    println(i)
+}
+
